2d1c0e289727470a5b9403e96fd9361651baaa78
Lucas Gabrielson <>
2021-03-08 17:55:35 -0600

    completed base mode

--- scripts/client.js
+++ scripts/client.js
@@ -1,8 +1,12 @@
 $( document ).ready( onReady );
 
+let contestant = '';
+
 function onReady() {
     displayToDOM(people);
     randomNameToDom(people);
+    console.log('here');
+    $( '#image-blocks' ).on( 'click', '.githubImages', foundRightImage);
 } // end onReady
 console.log('Here are all the available people:', people);
 
@@ -13,7 +17,7 @@ function displayToDOM( array ) {
     // append each person's profile picture to the DOM
     $( '#image-blocks' ).append(`
     <div>
-    <img class="githubImages" data-id=${i} src="https://github.com/${array[i].githubUsername}.png?size=250" alt="Profile image of ${array[i].name}">
+    <img class="githubImages" data-name=${array[i].name} src="https://github.com/${array[i].githubUsername}.png?size=250" alt="Profile image of ${array[i].name}">
     </div>
     `);
   } // end for
@@ -21,11 +25,30 @@ function displayToDOM( array ) {
 
 function randomNameToDom(array) {
   console.log( 'in randomName' );
-  $( '#random-name' ).append(`${array[randomNumber(0, 10)].name}`)
+  let random = [randomNumber(0, 9)];
+  $( '#contestant' ).append(
+    `
+    <h3>Select <span id="random-name" data-id="${random}">${array[random].name}</span> from the images</h3>
+    `
+  );
+
+  contestant = array[random].name;
 } // end randomNameToDOM
 
 function randomNumber(min, max){
   return Math.floor(Math.random() * (1 + max - min) + min);
 }
 
+function foundRightImage() {
+  console.log( 'in foundRight' );
+  console.log($(this).data( 'name'));
+  if( $( this ).data( 'name' ) === contestant) {
+    alert( `Success! That is ${$( this ).data( 'name' )}`)
+  }
+  else {
+    alert( `Do Better!`)
+  }
+
+  
+} // end foundRightImage
 
